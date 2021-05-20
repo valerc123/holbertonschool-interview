d4175bace4c41d6ba1302751d17135856bd861c1
valerc123 <valentina.rua123@gmail.com>
2021-05-19 22:16:54 -0500

    first attempt

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,74 @@
+#include "sandpiles.h"
+
+/**
+ * isStable - Check if the grid is stable, 
+ * stable means that each cell is less than 4
+ * @grid: 3x3 grid
+ * Return: 1 if is stable, otherwise return 0
+ *
+ */
+int isStable(int grid1[3][3]){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if (grid1[i][j] > 3){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+void add_sandpiles(int grid1[3][3], int grid2[3][3]){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            grid1[i][j] = grid1[i][j] + grid2[i][j];
+        }
+    }
+}
+
+void topple(int grid1[3][3]) {
+  int nextpiles[3][3];
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      int num = grid1[x][y];
+      if (num < 4) {
+        nextpiles[x][y] = grid1[x][y];
+      }
+    }
+  }
+
+  for (int x = 0; x < 3; x++) {
+    for (int y = 0; y < 3; y++) {
+      int num = grid1[x][y];
+      if (num >= 4) {
+        nextpiles[x][y] += (num - 4);
+        // down
+        if (x+1 < 3)
+          nextpiles[x+1][y]++;
+        // up
+        if (x-1 >= 0)
+          nextpiles[x-1][y]++;
+        // Right
+        if (y+1 < 3)
+          nextpiles[x][y+1]++;
+        //left 
+        if (y-1 >= 0)
+          nextpiles[x][y-1]++;
+      }
+    }
+  }
+
+  grid1 = nextpiles;
+}
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]){
+    add_sandpiles(grid1, grid2);
+   /* if (!isStable(grid1)){
+        print_grid(grid1);
+    }*/
+    while(!isStable(grid1)){
+        //topple method
+        topple(grid1);
+        /*if (!isStable(grid1)){
+			print_grid(grid1);
+        }*/
+    }
+}

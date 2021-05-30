36d9db444130616f9033ef1186a8a7ca88a69fac
valerc123 <valentina.rua123@gmail.com>
2021-05-30 01:19:15 -0500

    check if singly linked list is  palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: head of a singly linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+    return checkPalindrome(head, *head);
+}
+int checkPalindrome(listint_t **left, listint_t *right)
+{
+    // base case
+    if (right == NULL) {
+        return 1;
+    }
+ 
+    int result = checkPalindrome(left, right->next) &&
+            ((*left)->n == right->n);
+    (*left) = (*left)->next;
+ 
+    return result;
+}

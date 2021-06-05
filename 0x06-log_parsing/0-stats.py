0fd35920937737defa342b79ed84fbacbd81baee
valerc123 <valentina.rua123@gmail.com>
2021-06-05 13:36:00 -0500

    fix log parsign

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
 
 """Reads stdin line by line & computes metrics."""
-from time import sleep
-import random
 import sys
 
-if __name__ == "__main__":
-    lines = 0
-    fileSize = 0
-    status = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
+lines = 0
+fileSize = 0
+status = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-    try:
-        for line in sys.stdin:
-            arr = line.split(" ")
+try:
+    for line in sys.stdin:
+        arguments = line.split(" ")
+        if len(arguments) > 2:
+            code = arguments[-2]
+            fileSize += int(arguments[-1])
             lines += 1
-            fileSize += int(arr[-1])
-            code = arr[-2]
-            if(code in line):
+            if code in status:
                 status[code] += 1
             if lines == 10:
-                lines = 0
                 print(f"File size: {fileSize}")
-                for key, value in status.items():
+                for key, value in sorted(status.items()):
                     if value != 0:
                         print(f"{key}: {value}")
-                    fileSize = 0
-                status = dict.fromkeys(status, 0)
-    except KeyboardInterrupt as err:
-        print(f"File size: {fileSize}")
-        for key, value in status.items():
-            if value != 0:
-                print(f"{key}: {value}")
-    finally:
-        sleep(random.random())
+                line = 0
+except KeyboardInterrupt:
+    pass
+finally:
+    print(f"File size: {fileSize}")
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print(f"{key}: {value}")

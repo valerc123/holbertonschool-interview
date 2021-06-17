2d2501100dc8b96a58b287753e90a91f65a4cd5c
valerc123 <valentina.rua123@gmail.com>
2021-06-17 01:26:07 -0500

    remove global variable

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,44 +1,4 @@
 #include "palindrome.h"
-
-/**
- * num_digit - function returns number of digits of a number
- *
- * @num: number to be checked
- *
- * Return: number of digits
- */
-unsigned long num_digit(unsigned long num)
-{
-	unsigned long count = 0;
-
-	while (num > 0)
-	{
-		num /= 10;
-		count++;
-	}
-	return (count);
-}
-
-/**
- * reverse - revert a string
- *
- * @str: strign to be reverted
- *
- * Return: string reverted
- */
-char *reverse(char *str)
-{
-	static int i;
-	static char revstr[100];
-
-	if (*str)
-	{
-		reverse(str + 1);
-		revstr[i++] = *str;
-	}
-	return (revstr);
-}
-
 /**
  * is_palindrome - function that checks
  * whether or not a given unsigned integer is a palindrome
@@ -49,15 +9,18 @@ char *reverse(char *str)
  */
 int is_palindrome(unsigned long n)
 {
-	unsigned long digit = num_digit(n);
-	char buffer[sizeof(unsigned long) * digit + 1];
-	char copy[sizeof(unsigned long) * digit + 1];
-	char *revstr;
+	int reversedN = 0, remainder, originalN;
 
-	sprintf(buffer, "%lu", n);
-	strcpy(copy, buffer);
-	revstr = reverse(copy);
-	if (!strcmp(buffer, revstr))
+	originalN = n;
+
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reversedN = reversedN * 10 + remainder;
+		n /= 10;
+	}
+
+	if (originalN == reversedN)
 		return (1);
 	else
 		return (0);

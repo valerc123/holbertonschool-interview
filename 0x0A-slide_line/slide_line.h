1906290dbf33ac27e5550babd9b8e95c76a79e0f
valerc123 <valentina.rua123@gmail.com>
2021-12-07 03:53:56 -0500

    slide line

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,12 @@
+#ifndef SLIDE_LINE_H
+#define SLIDE_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int slide_line(int *line, size_t size, int direction);
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+
+#endif

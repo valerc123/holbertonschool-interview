5fae0a553d4c4335846680a604236c5b243d1045
valerc123 <valentina.rua123@gmail.com>
2021-12-07 05:03:40 -0500

    utf8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Module that validates valid UTF-8 encoding """
+
+
+def validUTF8(data):
+    """ Validates UTF-8 encoding """
+
+    string = bytes([i & 255 for i in data])
+    print(string)
+    try:
+        string.decode("UTF-8", "strict")
+        return True
+    except Exception:
+        return False

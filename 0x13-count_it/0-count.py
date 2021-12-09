52b56ae048804c9a26baa0f33dbe73edc0805ccc
valerc123 <valentina.rua123@gmail.com>
2021-12-09 10:39:15 -0500

    count it

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,53 +1,70 @@
 #!/usr/bin/python3
-""" Count it!
+""" Module that holds count it,
+    recursive function that queries the Reddit API,
+    parses the title of all hot articles,
+    and prints a sorted count of given keywords
 """
 import requests
 
+reddit_url = 'https://api.reddit.com/r/'
+aux_url = '{}/hot.json?after={}'
 
-def hot_dict_fill(response, word_list, hot_dict, after):
-    """ Fill hot dict increment count words
+
+def recurse(subreddit, new_list=[], after=None):
+    """ Method that Get list
     """
-    titles = response.json().get("data").get("children")
-    for title in titles:
-        for hot_word in word_list:
-            title_post = title.get("data").get("title")
-            if title_post:
-                words_in_title = title_post.split()
-                for word_title in words_in_title:
-                    if hot_word.lower() == word_title.lower():
-                        hot_dict[hot_word] += 1
-    if not after:
-        for k, v in sorted(hot_dict.items(),
-                           key=lambda items: items[1],
-                           reverse=True):
-            if v != 0:
-                print("{}: {}".format(k, v))
-
-
-def count_words(subreddit, word_list, after=None, hot_dict={}):
-    """Write a recursive function that queries the Reddit API, parses the
-    title of all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces. Javascript should count as
-    javascript, but java should not).
-    """
-    headers = {'User-Agent': 'DiegoOrejuela'}
-    params = {"limit": 100, 'after': after}
-    response = requests.get("https://www.reddit.com/r/{}/hot/.json".
-                            format(subreddit), headers=headers, params=params)
-
-    if len(hot_dict) == 0:
-        for hot_word in word_list:
-            hot_dict[hot_word] = 0
-
-    if response:
-        after_response = response.json().get("data").get("after")
-        if after_response:
-            count_words(subreddit, word_list,
-                        after=after_response, hot_dict=hot_dict)
-            hot_dict_fill(response, word_list, hot_dict, after)
-            return(hot_dict)
-        else:
-            hot_dict_fill(response, word_list, hot_dict, after)
-            return(hot_dict)
+    if after is not None:
+        url = reddit_url + aux_url.format(subreddit, after)
     else:
-        return(None)
+        url = reddit_url + '{}/hot.json'.format(subreddit)
+    header = {'user-agent': 'mr. big'}
+
+    result = requests.get(url, headers=header, allow_redirects=False)
+    if result.status_code != 200:
+        return None
+    try:
+        resJson = result.json()
+    except Exception:
+        return None
+
+    if resJson.get('message') == 'Not Found':
+        return
+
+    elements = resJson.get('data').get('children')
+    new_list += [element.get('data').get('title') for element in elements]
+    after = resJson.get('data').get('after')
+    if after is not None:
+        return recurse(subreddit, new_list, after)
+    else:
+        return new_list
+
+
+def print_new_words(word_list, new_list):
+    """ Method that print words
+    """
+    new_dict = {}
+    for word in word_list:
+        x = 0
+        for title in new_list:
+            j = title.lower().split()
+            if word.lower().strip() in j:
+                x += j.count(word.lower().strip())
+
+        if word.lower() in new_dict:
+            new_dict[word.lower()] += x
+        else:
+            new_dict.update({word.lower(): x})
+
+    for key, value in sorted(new_dict.items(), key=lambda x: (-x[1], x[0])):
+        if new_dict.get(key) != 0:
+            print("{}: {}".format(key, value))
+
+
+def count_words(subreddit, word_list):
+    """ Method that counts words
+    """
+    new_list = recurse(subreddit)
+    if new_list is None:
+        return
+
+    print_new_words(word_list, new_list)
